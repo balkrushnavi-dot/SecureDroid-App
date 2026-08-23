8fdb873a2fb912fc2aebfc630ca42ac748035bad
balkrushnavi-dot <balkrushnavi@gmail.com>
2026-08-23 12:41:36 +0530

    Update MainActivity.kt

--- android/app/src/main/java/org/securedroid/app/MainActivity.kt
+++ android/app/src/main/java/org/securedroid/app/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.securedroid.app
+package org.securedroid.app
 
 import android.os.Bundle
 import com.getcapacitor.BridgeActivity
 
 class MainActivity : BridgeActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Register your native security plugin so React/TypeScript can invoke it
         registerPlugin(SecureDroidPlugin::class.java)
         super.onCreate(savedInstanceState)
     }
