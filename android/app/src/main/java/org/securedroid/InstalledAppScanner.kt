1d9ca79eba05c868ec83694f513f7dd4ac48f693
balkrushnavi-dot <balkrushnavi@gmail.com>
2026-08-23 16:10:37 +0530

    Update InstalledAppScanner.kt

--- android/app/src/main/java/org/securedroid/InstalledAppScanner.kt
+++ android/app/src/main/java/org/securedroid/InstalledAppScanner.kt
@@ -1,41 +1,24 @@
-package org.securedroid.apps
+package org.securedroid
 
 import android.content.Context
-import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager
+import org.securedroid.apps.InstalledAppInfo
 
-data class AppInfoModel(
-    val packageName: String,
-    val appName: String,
-    val isSystemApp: Boolean,
-    val category: String
-)
+class InstalledAppScanner(
+    context: Context
+) {
 
-class InstalledAppScanner(private val context: Context) {
+    private val delegate =
+        org.securedroid.apps.InstalledAppScanner(context)
 
-    private val packageManager: PackageManager = context.packageManager
+    fun scan(): List<InstalledAppInfo> {
+        return delegate.scan()
+    }
 
-    fun getInstalledApplications(): List<AppInfoModel> {
-        val apps = mutableListOf<AppInfoModel>()
-        try {
-            val installedPackages = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
-            for (appInfo in installedPackages) {
-                val appName = packageManager.getApplicationLabel(appInfo).toString()
-                val isSystem = (appInfo.flags and ApplicationInfo.FLAG_SYSTEM) != 0
-                val category = if (isSystem) "System" else "User"
+    fun findPackage(packageName: String): InstalledAppInfo? {
+        return delegate.findPackage(packageName)
+    }
 
-                apps.add(
-                    AppInfoModel(
-                        packageName = appInfo.packageName,
-                        appName = appName,
-                        isSystemApp = isSystem,
-                        category = category
-                    )
-                )
-            }
-        } catch (e: Exception) {
-            // Handle query exceptions gracefully
-        }
-        return apps
+    fun isInstalled(packageName: String): Boolean {
+        return delegate.isInstalled(packageName)
     }
 }
