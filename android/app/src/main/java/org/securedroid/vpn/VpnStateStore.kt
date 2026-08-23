af18a4fe116998adca228d0507c090e5c953f81e
balkrushnavi-dot <balkrushnavi@gmail.com>
2026-08-23 16:40:25 +0530

    Create VpnStateStore.kt

---
+++ android/app/src/main/java/org/securedroid/vpn/VpnStateStore.kt
@@ -0,0 +1,17 @@
+package org.securedroid.vpn
+
+import java.util.concurrent.atomic.AtomicReference
+
+object VpnStateStore {
+
+    private val state =
+        AtomicReference(VpnState.DISCONNECTED)
+
+    fun get(): VpnState {
+        return state.get()
+    }
+
+    fun set(newState: VpnState) {
+        state.set(newState)
+    }
+}
