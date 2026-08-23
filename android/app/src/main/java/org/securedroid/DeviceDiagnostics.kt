4c8886e00674886dd25b2f8e574caaf26a3bcf89
balkrushnavi-dot <balkrushnavi@gmail.com>
2026-08-23 16:08:22 +0530

    Update DeviceDiagnostics.kt

--- android/app/src/main/java/org/securedroid/DeviceDiagnostics.kt
+++ android/app/src/main/java/org/securedroid/DeviceDiagnostics.kt
@@ -1,32 +1,16 @@
-package org.securedroid.diagnostics
+package org.securedroid
 
 import android.content.Context
-import android.os.Build
-import org.securedroid.admin.DevicePolicyManagerService
-import org.securedroid.security.KeyStoreManager
+import org.securedroid.diagnostics.DeviceDiagnosticsResult
 
-data class DiagnosticReport(
-    val deviceModel: String,
-    val androidVersion: String,
-    val sdkInt: Int,
-    val securityPatch: String,
-    val isDeviceOwner: Boolean,
-    val isHardwareKeystoreBacked: Boolean
-)
+class DeviceDiagnostics(
+    context: Context
+) {
 
-class DeviceDiagnostics(private val context: Context) {
+    private val delegate =
+        org.securedroid.diagnostics.DeviceDiagnostics(context)
 
-    private val devicePolicyService = DevicePolicyManagerService(context)
-    private val keyStoreManager = KeyStoreManager(context)
-
-    fun generateReport(): DiagnosticReport {
-        return DiagnosticReport(
-            deviceModel = "${Build.MANUFACTURER} ${Build.MODEL}",
-            androidVersion = Build.VERSION.RELEASE ?: "Unknown",
-            sdkInt = Build.VERSION.SDK_INT,
-            securityPatch = Build.VERSION.SECURITY_PATCH ?: "Unknown",
-            isDeviceOwner = devicePolicyService.isDeviceOwnerActive(),
-            isHardwareKeystoreBacked = keyStoreManager.isHardwareBacked()
-        )
+    fun run(): DeviceDiagnosticsResult {
+        return delegate.run()
     }
 }
